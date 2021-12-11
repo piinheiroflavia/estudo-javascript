eabce28840348e1cf32ce7db9cf17ced5d2222ab
Flavia Pinheio <flavia.p_17@hotmail.com>
2021-12-10 23:56:46 -0300

    ex condicões

---
+++ ex008..js
@@ -0,0 +1,6 @@
+ var vel = 70
+console.log(`a velocidade do seu carro é ${vel} km/hr`)
+if  (vel > 60 ){ //CONDIÇÃO SIMPLES
+console.log (`Você ultrapassou o limite de velocidade permitida. MULTADO!!!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
---
+++ ex09.js
@@ -0,0 +1,9 @@
+//CONDIÇÃO COMPOSTA
+var país = 'eua'
+console.log(`Vivendo em ${país}`)
+
+if (país != 'Brasil'){
+    console.log ('Você é Estrangeiro!!')
+} else {
+    console.log('Você é Brasileiro!!')
+}
